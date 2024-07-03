55da6ac8813f1d6d4b284e0b320aef1c5518b7cc
Soumik Banerjee <87870240+Soumiksb06@users.noreply.github.com>
2024-07-03 18:36:11 +0530

    Update improved_app.py

--- improved_app.py
+++ improved_app.py
@@ -4,12 +4,11 @@ from sentence_transformers import SentenceTransformer, util
 import chardet
 from detect_delimiter import detect
 import numpy as np
-from sklearn.manifold import TSNE
 import plotly.graph_objects as go
 import colorsys
 from sklearn.cluster import AgglomerativeClustering, KMeans
+import torch
 
-# Function to generate distinct colors in RGB format
 def generate_colors(num_colors):
     colors = []
     hue_values = np.linspace(0, 1, num_colors, endpoint=False)
@@ -22,7 +21,6 @@ def generate_colors(num_colors):
         colors.append(tuple(int(x * 255) for x in rgb))
     return colors
 
-# Function to calculate cluster coherence
 def calculate_cluster_coherence(embeddings, cluster_labels):
     unique_labels = np.unique(cluster_labels)
     coherences = []
@@ -35,17 +33,14 @@ def calculate_cluster_coherence(embeddings, cluster_labels):
             coherences.append(coherence)
     return coherences
 
-# Configuration
 st.title("Semantic Keyword Clustering Tool")
 st.write("Upload a CSV or XLSX file containing keywords for clustering.")
 
-# Sidebar for clustering method selection
 clustering_method = st.sidebar.selectbox(
     "Select Clustering Method",
     ["Community Detection", "Agglomerative", "K-means"]
 )
 
-# Conditional inputs based on clustering method
 if clustering_method == "Community Detection":
     cluster_accuracy = st.slider("Cluster Accuracy (0-100)", 0, 100, 80) / 100
     min_cluster_size = st.number_input("Minimum Cluster Size", min_value=1, max_value=100, value=3)
@@ -60,14 +55,10 @@ uploaded_file = st.file_uploader("Upload Keyword CSV or XLSX", type=["csv", "xls
 
 if uploaded_file:
     try:
-        acceptable_confidence = 0.8
-
-        # Read the uploaded file
         raw_data = uploaded_file.getvalue()
         detected = chardet.detect(raw_data)
-        encoding_type = detected['encoding']
-        if detected['confidence'] < acceptable_confidence:
-            encoding_type = 'utf-8'
+        encoding_type = detected['encoding'] if detected['confidence'] >= 0.8 else 'utf-8'
+        
         try:
             text_data = raw_data.decode(encoding_type)
         except UnicodeDecodeError:
@@ -76,13 +67,9 @@ if uploaded_file:
 
         firstline = text_data.splitlines()[0]
         
-        # Determine file type and read accordingly
         if uploaded_file.name.endswith('.csv'):
             delimiter_type = detect(firstline)
-            if delimiter_type:
-                df = pd.read_csv(uploaded_file, encoding=encoding_type, delimiter=delimiter_type)
-            else:
-                df = pd.read_csv(uploaded_file, encoding=encoding_type)
+            df = pd.read_csv(uploaded_file, encoding=encoding_type, delimiter=delimiter_type or ',')
         elif uploaded_file.name.endswith('.xlsx'):
             df = pd.read_excel(uploaded_file, engine='openpyxl')
 
@@ -94,20 +81,16 @@ if uploaded_file:
         if "Keyword" not in df.columns:
             st.error("Error! Please make sure your CSV or XLSX file contains a column named 'Keyword'!")
         else:
-            # Data validation
             st.write(f"Total rows: {len(df)}")
             st.write(f"Number of unique keywords: {df['Keyword'].nunique()}")
             st.write("Data types in 'Keyword' column:")
             st.write(df['Keyword'].apply(type).value_counts())
             
-            # Convert all values in the 'Keyword' column to strings
             df['Keyword'] = df['Keyword'].astype(str)
             
-            # Display a sample of the data
             st.write("Sample of the data (first 5 rows):")
             st.write(df.head())
 
-            # Clustering Process
             model = SentenceTransformer(transformer)
             corpus_set = set(df['Keyword'])
             corpus_set_all = corpus_set
@@ -126,13 +109,31 @@ if uploaded_file:
 
                 corpus_embeddings = model.encode(corpus_sentences, batch_size=256, show_progress_bar=True, convert_to_tensor=True)
                 
-                # Ensure corpus_embeddings is 2D
                 if len(corpus_embeddings.shape) == 1:
                     corpus_embeddings = corpus_embeddings.reshape(1, -1)
                 
                 if clustering_method == "Community Detection":
                     clusters = util.community_detection(corpus_embeddings, min_community_size=min_cluster_size, threshold=cluster_accuracy)
                     cluster_labels = np.array([i for i, cluster in enumerate(clusters) for _ in cluster])
+                    
+                    coherences = []
+                    for cluster in clusters:
+                        if len(cluster) > 1:
+                            cluster_embeddings = corpus_embeddings[cluster]
+                            centroid = torch.mean(cluster_embeddings, dim=0)
+                            distances = torch.norm(cluster_embeddings - centroid, dim=1)
+                            coherence = 1 / (1 + torch.mean(distances).item())
+                            coherences.append(coherence)
+                    
+                    if coherences:
+                        overall_coherence = np.mean(coherences)
+                        st.write(f"Overall Clustering Coherence: {overall_coherence:.4f}")
+                        st.write("Cluster Coherences:")
+                        for i, coherence in enumerate(coherences):
+                            st.write(f"Cluster {i+1}: {coherence:.4f}")
+                    else:
+                        st.write("Coherence: Not applicable (insufficient data)")
+                    
                 elif clustering_method == "Agglomerative":
                     max_clusters = len(corpus_sentences) // 4
                     while True:
@@ -158,9 +159,7 @@ if uploaded_file:
                         corpus_sentences_list.append(corpus_sentences[sentence_id])
                         cluster_name_list.append(f"Cluster {cluster_id + 1}")
 
-                df_new = pd.DataFrame(None)
-                df_new['Cluster Name'] = cluster_name_list
-                df_new["Keyword"] = corpus_sentences_list
+                df_new = pd.DataFrame({'Cluster Name': cluster_name_list, 'Keyword': corpus_sentences_list})
 
                 df_all.append(df_new)
                 have = set(df_new["Keyword"])
@@ -177,21 +176,9 @@ if uploaded_file:
                 df_new = pd.concat(df_all)
                 df = df.merge(df_new.drop_duplicates('Keyword'), how='left', on="Keyword")
 
-                df['Length'] = df['Keyword'].astype(str).map(len)
-                df = df.sort_values(by="Length", ascending=True)
-
-                df['Cluster Name'] = df.groupby('Cluster Name')['Keyword'].transform('first')
-                df.sort_values(['Cluster Name', "Keyword"], ascending=[True, True], inplace=True)
-
                 df['Cluster Name'] = df['Cluster Name'].fillna("no_cluster")
 
-                del df['Length']
-
-                col = df.pop("Keyword")
-                df.insert(0, col.name, col)
-
-                col = df.pop('Cluster Name')
-                df.insert(0, col.name, col)
+                df = df[['Cluster Name', 'Keyword']]
 
                 df.sort_values(["Cluster Name", "Keyword"], ascending=[True, True], inplace=True)
 
@@ -201,11 +188,9 @@ if uploaded_file:
                 st.write(f"Number of iterations: {iterations}")
                 st.write(f"Total unclustered keywords: {remaining}")
 
-                # Print number of clusters
                 st.write(f"Number of clusters: {len(df['Cluster Name'].unique())}")
 
-                # Calculate cluster coherences
-                if len(corpus_embeddings) > 1:
+                if clustering_method != "Community Detection" and len(corpus_embeddings) > 1:
                     cluster_coherences = calculate_cluster_coherence(corpus_embeddings.cpu().numpy(), cluster_labels)
                     overall_coherence = np.mean(cluster_coherences)
 
@@ -213,29 +198,20 @@ if uploaded_file:
                     st.write("Cluster Coherences:")
                     for i, coherence in enumerate(cluster_coherences):
                         st.write(f"Cluster {i+1}: {coherence:.4f}")
-                else:
-                    st.write("Coherence: Not applicable (insufficient data)")
 
-                # Save results with only 'Cluster' and 'Keywords' columns
                 result_df = df.groupby('Cluster Name')['Keyword'].apply(', '.join).reset_index()
                 result_df.columns = ['Cluster', 'Keywords']
 
-                # Display result dataframe
                 st.write(result_df)
 
-                # Generate 3D plot of clusters
-                # Assuming you have numerical embeddings or features for each keyword
-                # For demonstration, let's create random data
                 np.random.seed(42)
                 num_keywords = len(df['Keyword'])
-                embeddings_3d = np.random.randn(num_keywords, 3)  # Replace with your actual 3D embeddings
+                embeddings_3d = np.random.randn(num_keywords, 3)
 
-                # Generate colors for clusters
                 unique_clusters = df['Cluster Name'].unique()
                 num_clusters = len(unique_clusters)
                 cluster_colors = generate_colors(max(num_clusters, 100))
 
-                # Create a figure for the 3D scatter plot
                 fig_3d = go.Figure()
 
                 for i, cluster in enumerate(unique_clusters):
@@ -250,12 +226,11 @@ if uploaded_file:
                             color=f'rgb{cluster_colors[i]}',
                             opacity=0.8,
                         ),
-                        text=cluster_data['Keyword'],  # Display keyword names on hover
-                        hoverinfo='text',  # Show only text (keyword names) on hover
+                        text=cluster_data['Keyword'],
+                        hoverinfo='text',
                         name=cluster
                     ))
 
-                # Update layout for 3D scatter plot
                 fig_3d.update_layout(
                     width=800,
                     height=700,
@@ -268,10 +243,8 @@ if uploaded_file:
                     margin=dict(l=0, r=0, b=0, t=0)
                 )
 
-                # Display 3D scatter plot using Streamlit
                 st.plotly_chart(fig_3d, use_container_width=True)
 
-                # Download button for clustered keywords
                 csv_data_clustered = result_df.to_csv(index=False)
                 st.download_button(
                     label="Download Clustered Keywords",
@@ -280,15 +253,12 @@ if uploaded_file:
                     mime="text/csv"
                 )
 
-                # Display unclustered keywords and add download button
                 if remaining > 0:
                     st.write("Unclustered Keywords:")
                     st.write(list(corpus_set))
                     
-                    # Create a DataFrame of unclustered keywords
                     unclustered_df = pd.DataFrame(list(corpus_set), columns=['Unclustered Keyword'])
                     
-                    # Download button for unclustered keywords
                     csv_data_unclustered = unclustered_df.to_csv(index=False)
                     st.download_button(
                         label="Download Unclustered Keywords",
